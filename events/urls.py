9adb2fdafd5f22e343528d053f2a24d6b19e9afc
Sergio Infante <raulsergio9@gmail.com>
2019-03-14 07:37:14 -0500

    Remove endpoint unused urls for events

--- events/urls.py
+++ events/urls.py
@@ -5,8 +5,6 @@ from .views import event_featured_list, event_register_participant, event_detail
 app_name = 'events'
 
 urlpatterns = [
-    path('<int:event_id>/detail/', event_detail, name='event_detail'),
     path('featured/', event_featured, name='event_featured'),
-    path('featured/list/', event_featured_list, name='event_featured_list'),
     path('register/participant/<str:code>', event_register_participant, name='event_register_participant'),
 ]
