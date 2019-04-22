beab6fdb00243acaad17b5b6330e6090e4e588ce
Sergio Infante <raulsergio9@gmail.com>
2019-04-22 16:53:56 -0500

    Remove identity and identity validation endpoints

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import CustomAuthToken, user_logout, user_create, user_list
 from .views import user_validation, user_password_recovery_request, user_password_recovery_request_confirmation
 from .views import user_password_update, user_profile, user_profile_update
-from .views import user_identity, user_identity_validation
 
 
 app_name = 'users'
@@ -11,8 +10,6 @@ app_name = 'users'
 urlpatterns = [
     path('authenticate/', CustomAuthToken.as_view()),
     path('create/', user_create, name='user_create'),
-    path('identity/', user_identity, name='user_identity'),
-    path('identity/validation/', user_identity_validation, name='user_identity_validation'),
     path('list/', user_list, name='user_list'),
     path('logout/', user_logout, name='user_logout'),
     path('profile/', user_profile, name='user_profile'),
