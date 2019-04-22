a53b055256c5142a680ced7c12ecc82973310d3c
Sergio Infante <raulsergio9@gmail.com>
2019-04-22 17:00:43 -0500

    Remove send participant codes and send participant notification endpoints urls

--- events/urls.py
+++ events/urls.py
@@ -1,16 +1,11 @@
 from django.urls import path
 
 from .views import event_featured, registrant_list
-from .views import event_send_participant_codes
-from .views import event_featured_send_notification
+
 
 app_name = 'events'
 
 urlpatterns = [
     path('featured/', event_featured, name='event_featured'),
-    path('featured/participant/send/code/', event_send_participant_codes, name='event_send_participant_codes'),
-    path('featured/participant/send/notification/',
-         event_featured_send_notification,
-         name='event_featured_send_notification'),
     path('registrant/list/', registrant_list, name='registrant_list'),
 ]
