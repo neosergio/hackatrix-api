9a8aa64bb4ba0fc2efea8c34c203742daa73908a
Sergio Infante <raulsergio9@gmail.com>
2019-04-29 16:49:06 -0500

    Enable idea creation endpoint

--- ideas/urls.py
+++ ideas/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
+from .views import idea_creation
 from .views import idea_detail, idea_list_complete, idea_list_validated
 from .views import idea_validation_switch
 
 app_name = 'ideas'
 
 urlpatterns = [
+    path('create/', idea_creation, name='idea_creation'),
     path('<int:idea_id>/detail/', idea_detail, name='idea_detail'),
     path('<int:idea_id>/validation/switch/', idea_validation_switch, name='idea_validation_switch'),
     path('list/complete/', idea_list_complete, name='idea_list_complete'),
