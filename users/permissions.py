80f5a0d5c59c17221d718709f6365fed594d1e18
Sergio Infante <raulsergio9@gmail.com>
2019-05-12 15:36:06 -0500

    Add is_staff permission

--- users/permissions.py
+++ users/permissions.py
@@ -4,4 +4,10 @@ from rest_framework import permissions
 class IsModerator(permissions.BasePermission):
 
     def has_permission(self, request, view):
+        return request.user and request.user.is_moderator
+
+
+class IsStaff(permissions.BasePermission):
+
+    def has_permission(self, request, view):
         return request.user and request.user.is_staff
