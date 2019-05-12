58ae1739c2b82c1c77f13d58b469bbcd75d6491b
Sergio Infante <raulsergio9@gmail.com>
2019-05-12 07:35:28 -0500

    Avoid author as team member (is already one by default)

--- ideas/views.py
+++ ideas/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from constance import config
 from rest_framework import permissions, status
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError, PermissionDenied
 from rest_framework.response import Response
 
 from .models import Idea, IdeaTeamMember
@@ -16,6 +16,9 @@ from utils.pagination import StandardResultsSetPagination
 @api_view(['POST', ])
 @permission_classes((permissions.IsAuthenticated, ))
 def idea_creation(request):
+    """
+    Creates an idea
+    """
     serializer = IdeaCreationSerializer(data=request.data)
     if serializer.is_valid(raise_exception=True):
         title = serializer.validated_data['title']
@@ -47,6 +50,9 @@ def idea_add_team_member(request, idea_id):
     if serializer.is_valid(raise_exception=True):
         code_to_validate = serializer.validated_data['registrant_qr_code']
         registrant = get_object_or_404(Registrant, code=code_to_validate)
+        registrant_ideas = Idea.objects.filter(is_valid=True, author=registrant)
+        if len(registrant_ideas) > 0:
+            raise PermissionDenied("Participante es autor de una idea")
 
         if len(IdeaTeamMember.objects.filter(idea=idea)) < idea.max_number_of_participants:
             try:
@@ -88,10 +94,11 @@ def idea_add_team_member_list(request, idea_id):
     if serializer.is_valid(raise_exception=True):
         idea_team_members = serializer.validated_data['idea_team_members']
         for team_member in idea_team_members:
-            print(team_member['registrant_qr_code'])
             registrant = get_object_or_404(Registrant, code=team_member['registrant_qr_code'])
-
-            if len(IdeaTeamMember.objects.filter(idea=idea)) < idea.max_number_of_participants:
+            registrant_ideas = Idea.objects.filter(is_valid=True, author=registrant)
+            if len(registrant_ideas) > 0:
+                pass
+            elif len(IdeaTeamMember.objects.filter(idea=idea)) < idea.max_number_of_participants:
                 try:
                     IdeaTeamMember.objects.create(idea=idea, member=registrant)
                 except Exception as e:
@@ -107,6 +114,9 @@ def idea_add_team_member_list(request, idea_id):
 @api_view(['GET', ])
 @permission_classes((permissions.IsAuthenticated, ))
 def idea_detail(request, idea_id):
+    """
+    Returns an idea detail
+    """
     idea = get_object_or_404(Idea, pk=idea_id)
     serializer = IdeaSerializer(idea)
     return Response(serializer.data, status=status.HTTP_200_OK)
@@ -115,6 +125,9 @@ def idea_detail(request, idea_id):
 @api_view(['GET', ])
 @permission_classes((permissions.IsAuthenticated, ))
 def idea_list_complete(request):
+    """
+    Returns full idea list
+    """
     event = Event.objects.filter(is_active=True, is_featured=True).first()
     ideas = Idea.objects.filter(event=event)
     if request.GET.get('page') or request.GET.get('per_page'):
@@ -130,6 +143,9 @@ def idea_list_complete(request):
 @api_view(['GET', ])
 @permission_classes((permissions.IsAuthenticated, ))
 def idea_list_validated(request):
+    """
+    Returns full idea validated list
+    """
     event = Event.objects.filter(is_active=True, is_featured=True).first()
     ideas = Idea.objects.filter(event=event, is_valid=True)
     if request.GET.get('page') or request.GET.get('per_page'):
@@ -145,6 +161,9 @@ def idea_list_validated(request):
 @api_view(['PATCH', ])
 @permission_classes((IsModerator, ))
 def idea_validation_switch(request, idea_id):
+    """
+    Switches idea is_valid flag between false or true
+    """
     idea = get_object_or_404(Idea, pk=idea_id)
     if idea.is_valid:
         idea.is_valid = False
