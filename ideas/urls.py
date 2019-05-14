b8b55d12bea19f4d56881e2e1887ae976242265b
Sergio Infante <raulsergio9@gmail.com>
2019-05-14 10:30:00 -0500

    Add endpoint to get idea from registrant id as an author

--- ideas/urls.py
+++ ideas/urls.py
@@ -5,11 +5,13 @@ from .views import idea_add_team_member, idea_add_team_member_list
 from .views import idea_remove_team_member, idea_remove_team_member_list
 from .views import idea_detail, idea_list_complete, idea_list_validated
 from .views import idea_validation_switch
+from .views import author_idea_list
 
 app_name = 'ideas'
 
 urlpatterns = [
     path('create/', idea_creation, name='idea_creation'),
+    path('author/<int:registrant_id>', author_idea_list, name='author_idea_list'),
     path('<int:idea_id>/add/team/member/', idea_add_team_member, name='idea_add_team_member'),
     path('<int:idea_id>/remove/team/member/', idea_remove_team_member, name='idea_remove_team_member'),
     path('<int:idea_id>/add/team/list/', idea_add_team_member_list, name='idea_add_team_member_list'),
