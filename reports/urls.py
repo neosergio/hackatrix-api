cc3af09508aca5868a4b122bcdfff0c7362604f7
Sergio Infante <raulsergio9@gmail.com>
2019-05-29 21:25:40 -0500

    Add project assessment report

--- reports/urls.py
+++ reports/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import index, attendance_list
+from .views import index, attendance_list, project_assessment
 
 
 app_name = 'reports'
@@ -8,4 +8,5 @@ app_name = 'reports'
 urlpatterns = [
     path('', index, name='index'),
     path('attendance/list/', attendance_list, name='attendance_list'),
+    path('assessment/project/', project_assessment, name='project_assessment'),
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from assessments.models import ProjectAssessment
 from events.models import RegistrantAttendance
 
 
@@ -14,3 +15,10 @@ def attendance_list(request):
     attendances = RegistrantAttendance.objects.all()
     context = {'attendances': attendances}
     return render(request, 'attendance_list.html', context)
+
+
+@login_required()
+def project_assessment(request):
+    assessments = ProjectAssessment.objects.all()
+    context = {'assessments': assessments}
+    return render(request, 'project_assessment.html', context)
