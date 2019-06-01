101bc13af702615241d575c58569652df6ccdaaa
Sergio Infante <raulsergio9@gmail.com>
2019-06-01 00:48:14 -0500

    Refactor attendance report in order to choose them by id

--- reports/urls.py
+++ reports/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import index, attendance_list
+from .views import index, attendance_list, attendance_list_by_id, attendance_items
 from .views import project_assessment_by_jury, project_assessment_by_evaluation_committee, idea_list
 from .views import registrant_list_by_project, registrant_assessment, registrant_list
 
@@ -9,6 +9,8 @@ app_name = 'reports'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('attendance/', attendance_items, name='attendance_items'),
+    path('attendance/<int:attendance_id>/list/', attendance_list_by_id, name='attendance_list_by_id'),
     path('attendance/list/', attendance_list, name='attendance_list'),
     path('project/list/', idea_list, name='idea_list'),
     path('project/teams/', registrant_list_by_project, name='registrant_list_by_project'),
--- reports/views.py
+++ reports/views.py
@@ -1,9 +1,9 @@
 from constance import config
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 
 from assessments.models import ProjectAssessment, RegistrantAssessment
-from events.models import Registrant, RegistrantAttendance
+from events.models import Attendance, Registrant, RegistrantAttendance
 from ideas.models import IdeaTeamMember, Idea
 
 
@@ -13,9 +13,30 @@ def index(request):
 
 
 @login_required()
+def attendance_items(request):
+    if config.DISPLAY_REPORTS:
+        attendances = Attendance.objects.all()
+        context = {'attendances': attendances}
+    else:
+        context = dict()
+    return render(request, 'attendances.html', context)
+
+
+@login_required()
 def attendance_list(request):
     if config.DISPLAY_REPORTS:
         attendances = RegistrantAttendance.objects.all()
+        context = {'attendances': attendances}
+    else:
+        context = dict()
+    return render(request, 'attendance_list.html', context)
+
+
+@login_required()
+def attendance_list_by_id(request, attendance_id):
+    if config.DISPLAY_REPORTS:
+        attendance = get_object_or_404(Attendance, pk=attendance_id)
+        attendances = RegistrantAttendance.objects.filter(attendance=attendance)
         context = {'attendances': attendances}
     else:
         context = dict()
