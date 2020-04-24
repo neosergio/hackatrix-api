7592668a6e3ac9c44da31213d211eb53a421e936
Sergio Infante <raulsergio9@gmail.com>
2020-04-24 09:39:47 -0500

    Comment user endpoints related to password recovery

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import CustomAuthToken, user_logout, user_create, user_list
-from .views import user_validation, user_password_recovery_request, user_password_recovery_request_confirmation
-from .views import user_password_update, user_profile, user_profile_update
-
+from .views import CustomAuthToken
+from .views import user_create
+from .views import user_list
+from .views import user_logout
+# from .views import user_validation, user_password_recovery_request, user_password_recovery_request_confirmation
+from .views import user_password_update
+from .views import user_profile
+from .views import user_profile_update
 
 app_name = 'users'
 
@@ -14,12 +18,12 @@ urlpatterns = [
     path('logout/', user_logout, name='user_logout'),
     path('profile/', user_profile, name='user_profile'),
     path('profile/update/', user_profile_update, name='user_profile_update'),
-    path('reset/password/',
-         user_password_recovery_request,
-         name='user_password_recovery_request'),
-    path('reset/password/<uuid:user_uuid>/',
-         user_password_recovery_request_confirmation,
-         name='user_password_recovery_request_confirmation'),
+    # path('reset/password/',
+    #     user_password_recovery_request,
+    #     name='user_password_recovery_request'),
+    # path('reset/password/<uuid:user_uuid>/',
+    #     user_password_recovery_request_confirmation,
+    #     name='user_password_recovery_request_confirmation'),
     path('update/password/', user_password_update, name='user_password_update'),
-    path('validate/<uuid:user_uuid>/', user_validation, name='user_validation'),
+    # path('validate/<uuid:user_uuid>/', user_validation, name='user_validation'),
 ]
