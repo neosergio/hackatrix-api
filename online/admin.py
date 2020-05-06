ca10680f35661f9fc12e80e5a0a12bf823bda1d4
Sergio Infante <raulsergio9@gmail.com>
2020-05-06 11:46:09 -0500

    Add team admin functionalities and test data in fixtures

--- online/admin.py
+++ online/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from import_export.admin import ImportExportMixin
 
 from .models import CategoryScore
 from .models import Comment
@@ -9,13 +10,22 @@ from .models import TeamMember
 from .models import Evaluator
 
 
-class EvaluationCommitteeAdmin(admin.ModelAdmin):
+class EvaluationCommitteeAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = ('name', 'is_evaluation_closed', 'is_active')
+
+
+class TeamAdmin(ImportExportMixin, admin.ModelAdmin):
+    list_display = ('name',
+                    'project',
+                    'project_description',
+                    'total_score',
+                    'evaluation_committee',
+                    'is_active')
 
 
 admin.site.register(CategoryScore)
 admin.site.register(Comment)
-admin.site.register(Team)
+admin.site.register(Team, TeamAdmin)
 admin.site.register(TeamMember)
 admin.site.register(EvaluationCommittee, EvaluationCommitteeAdmin)
 admin.site.register(Evaluation)
